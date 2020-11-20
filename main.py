288df6b222e906aa9c3d2b2c26a4a39d90f8bf86
mat barb <mattia.barbaresi@gmail.com>
2020-11-20 19:40:37 +0100

    changed calls order in main

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from genetic_algorithm.genetic_init import init
 if __name__ == "__main__":
     now = datetime.now()
     time_now = now.strftime("%Y%m%d-%H.%M")
-    for random_seed in [100]:  # for random_seed in [100, 330, 42]:
-        for generations in [50]:  # for generations in [1000,2000]:
-            for evaluation_method in [0]:  # 0-fitness, 1- novelty, 2-fitness and novelty
-                for repertoire_index in [1, 2, 3, 4, 5, 6, 7]:  # for repertoireIndex in [0..7]:
-                    for dissim_thresh in [0.45, 0.55]:
-                        for fitness_thresh in [0.5, 0.6]:
-                            init(number_of_generations=generations,  # generations,
-                                 repertoire_index=repertoire_index,  # repertoire_index,
+    for repertoire_index in [0,1,2,3,4,5,6,7]:  # for repertoireIndex in [0..7]:
+        for generations in [50]:
+            for random_seed in [7, 46, 101, 171]:
+                for fitness_thresh in [0.5, 0.53, 0.57]:
+                    for dissim_thresh in [0.45, 0.5, 0.53, 0.55]:
+                        for evaluation_method in [0, 1, 2]:  # 0-fitness, 1- novelty, 2-fitness and novelty
+                            init(number_of_generations=generations,
+                                 repertoire_index=repertoire_index,
                                  evaluation_method_index=evaluation_method,
                                  random_seed=random_seed,
                                  dissim_threshold=dissim_thresh,
                                  fitness_threshold=fitness_thresh)
+
+
